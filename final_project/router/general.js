390193d97136795905e24144b72cc0632caffd45
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-10-29 00:43:21 +0530

    feat: add feature upto task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,16 +11,22 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  // Retrieve ISBN from request parameters
+  const isbn = req.params.isbn;
+
+  // Check if the book exists in the database
+  if (books[isbn]) {
+    return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
